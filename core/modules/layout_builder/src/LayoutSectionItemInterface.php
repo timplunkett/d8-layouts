2b5ff7da850141cc11083544129c171d23a3f244
Tim Plunkett <git@plnktt.com>
2017-04-30 14:36:28 -0400

    Extract an interface for layout section field type.

---
+++ core/modules/layout_builder/src/LayoutSectionItemInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\layout_builder;
+
+use Drupal\Core\Field\FieldItemInterface;
+
+/**
+ * Defines an interface for the layout section field item.
+ *
+ * @property string layout
+ * @property array[] section
+ */
+interface LayoutSectionItemInterface extends FieldItemInterface {
+
+}
