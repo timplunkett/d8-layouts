0b5510f12255526b775992f3af2ac650cd46711c
Tim Plunkett <git@plnktt.com>
2017-05-08 15:53:11 -0400

    Refactor the first pass of routing and UI code.

--- core/modules/layout_builder/src/Access/LayoutSectionAccessCheck.php
+++ core/modules/layout_builder/src/Access/LayoutSectionAccessCheck.php
@@ -8,13 +8,9 @@ use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Route;
 
 /**
- * Provides an access checker for node revisions.
- *
- * @ingroup node_access
+ * @todo.
  */
 class LayoutSectionAccessCheck implements AccessInterface {
 
@@ -26,38 +22,43 @@ class LayoutSectionAccessCheck implements AccessInterface {
   protected $entityTypeManager;
 
   /**
-   * LayoutSectionAccessCheck constructor.
+   * Constructs a new LayoutSectionAccessCheck.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
-    $this->entityTypeManager = $entityTypeManager;
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
   }
-
 
   /**
    * Checks routing access to layout for the entity.
    *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The request object.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The currently logged in account.
+   *
    * @return \Drupal\Core\Access\AccessResultInterface
-   * The access result.
+   *   The access result.
    */
-  public function access(Request $request, AccountInterface $account) {
-    $entity = $request->attributes->get('entity');
-    $has_field = $request->attributes->get('field_name');
-    // If we don't have an entity forbid access.
-    if (empty($entity) || empty($has_field)) {
+  public function access(RouteMatchInterface $route_match, AccountInterface $account) {
+    $entity = $route_match->getParameter('layout_section_entity');
+    $field_name = $route_match->getParameter('layout_section_field_name');
+    // If we don't have an entity, forbid access.
+    if (empty($entity) || empty($field_name)) {
       return AccessResult::forbidden();
     }
+
     // If the entity isn't fieldable, forbid access.
-    if (!$entity instanceof FieldableEntityInterface) {
-      return AccessResult::forbidden()->addCacheableDependency($entity);
+    if (!$entity instanceof FieldableEntityInterface || !$entity->hasField($field_name)) {
+      $access = AccessResult::forbidden();
     }
-    return AccessResult::allowedIf($account->hasPermission('configure any layout'))->addCacheableDependency($entity);
+    else {
+      $access = AccessResult::allowedIf($account->hasPermission('configure any layout'));
+    }
+
+    return $access->addCacheableDependency($entity);
   }
 
 }
--- core/modules/layout_builder/src/Routing/LayoutBuilderRouteEnhancer.php
+++ core/modules/layout_builder/src/Routing/LayoutBuilderRouteEnhancer.php
@@ -1,32 +1,34 @@
 <?php
 
-
 namespace Drupal\layout_builder\Routing;
-
 
 use Drupal\Core\Routing\Enhancer\RouteEnhancerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
 
-class LayoutBuilderRouteEnhancer implements RouteEnhancerInterface{
+/**
+ * @todo.
+ */
+class LayoutBuilderRouteEnhancer implements RouteEnhancerInterface {
 
   /**
    * {@inheritdoc}
    */
   public function applies(Route $route) {
-    return ($route->hasOption('_layout_builder'));
+    return $route->hasOption('_layout_builder');
   }
 
   /**
    * {@inheritdoc}
    */
   public function enhance(array $defaults, Request $request) {
-    $entity_type_id = $defaults['_route_object']->getOption('_layout_builder');
-    $defaults['entity'] = $defaults[$entity_type_id];
+    // Copy the entity by reference so that any changes are reflected.
+    $defaults['layout_section_entity'] = &$defaults[$defaults['entity_type_id']];
+
     /** @var \Drupal\Core\Field\FieldItemListInterface $field */
-    foreach ($defaults['entity'] as $field) {
+    foreach ($defaults['layout_section_entity'] as $field) {
       if ($field->getFieldDefinition()->getType() == 'layout_section') {
-        $defaults['field_name'] = $field->getFieldDefinition()->getName();
+        $defaults['layout_section_field_name'] = $field->getFieldDefinition()->getName();
         break;
       }
     }
