821970785f7bb2d61d91b43fae515fce1e84f4f8
Tim Plunkett <git@plnktt.com>
2017-05-08 15:51:01 -0400

    Move layout section building to a dedicated service.

--- core/modules/layout_builder/src/Plugin/Field/FieldFormatter/LayoutSectionFormatter.php
+++ core/modules/layout_builder/src/Plugin/Field/FieldFormatter/LayoutSectionFormatter.php
@@ -2,18 +2,11 @@
 
 namespace Drupal\layout_builder\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Block\BlockManagerInterface;
-use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
-use Drupal\Core\Layout\LayoutPluginManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Plugin\Context\ContextHandlerInterface;
-use Drupal\Core\Plugin\Context\ContextRepositoryInterface;
-use Drupal\Core\Plugin\ContextAwarePluginInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\layout_builder\LayoutSectionItemInterface;
+use Drupal\layout_builder\LayoutSectionBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -30,53 +23,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LayoutSectionFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
 
   /**
-   * The current user.
+   * The layout section builder.
    *
-   * @var \Drupal\Core\Session\AccountInterface
+   * @var \Drupal\layout_builder\LayoutSectionBuilder
    */
-  protected $account;
-
-  /**
-   * The layout plugin manager.
-   *
-   * @var \Drupal\Core\Layout\LayoutPluginManager
-   */
-  protected $layoutPluginManager;
-
-  /**
-   * The block plugin manager.
-   *
-   * @var \Drupal\Core\Block\BlockManagerInterface
-   */
-  protected $blockManager;
-
-  /**
-   * The plugin context handler.
-   *
-   * @var \Drupal\Core\Plugin\Context\ContextHandlerInterface
-   */
-  protected $contextHandler;
-
-  /**
-   * The context manager service.
-   *
-   * @var \Drupal\Core\Plugin\Context\ContextRepositoryInterface
-   */
-  protected $contextRepository;
+  protected $builder;
 
   /**
    * Constructs a LayoutSectionFormatter object.
    *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The current user.
-   * @param \Drupal\Core\Layout\LayoutPluginManagerInterface $layoutPluginManager
-   *   The layout plugin manager.
-   * @param \Drupal\Core\Block\BlockManagerInterface $blockManager
-   *   THe block plugin manager.
-   * @param \Drupal\Core\Plugin\Context\ContextHandlerInterface $context_handler
-   *   The ContextHandler for applying contexts to conditions properly.
-   * @param \Drupal\Core\Plugin\Context\ContextRepositoryInterface $context_repository
-   *   The lazy context repository service.
+   * @param \Drupal\layout_builder\LayoutSectionBuilder $builder
+   *   The layout section builder.
    * @param string $plugin_id
    *   The plugin ID for the formatter.
    * @param mixed $plugin_definition
@@ -92,12 +49,8 @@ class LayoutSectionFormatter extends FormatterBase implements ContainerFactoryPl
    * @param array $third_party_settings
    *   Any third party settings.
    */
-  public function __construct(AccountInterface $account, LayoutPluginManagerInterface $layoutPluginManager, BlockManagerInterface $blockManager, ContextHandlerInterface $context_handler, ContextRepositoryInterface $context_repository, $plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings) {
-    $this->account = $account;
-    $this->layoutPluginManager = $layoutPluginManager;
-    $this->blockManager = $blockManager;
-    $this->contextHandler = $context_handler;
-    $this->contextRepository = $context_repository;
+  public function __construct(LayoutSectionBuilder $builder, $plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings) {
+    $this->builder = $builder;
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
   }
 
@@ -106,11 +59,7 @@ class LayoutSectionFormatter extends FormatterBase implements ContainerFactoryPl
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-      $container->get('current_user'),
-      $container->get('plugin.manager.core.layout'),
-      $container->get('plugin.manager.block'),
-      $container->get('context.handler'),
-      $container->get('context.repository'),
+      $container->get('layout_builder.builder'),
       $plugin_id,
       $plugin_definition,
       $configuration['field_definition'],
@@ -127,47 +76,12 @@ class LayoutSectionFormatter extends FormatterBase implements ContainerFactoryPl
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
+    /** @var \Drupal\layout_builder\LayoutSectionItemInterface[] $items */
     foreach ($items as $delta => $item) {
-      $elements[$delta] = $this->buildSection($item);
+      $elements[$delta] = $this->builder->buildSection($item->layout, $item->section);
     }
 
     return $elements;
-  }
-
-  /**
-   * Builds the render array for the layout section.
-   *
-   * @param \Drupal\layout_builder\LayoutSectionItemInterface $item
-   *   The layout section item.
-   *
-   * @return array
-   *   A render array for the field item.
-   */
-  protected function buildSection(LayoutSectionItemInterface $item) {
-    $cacheability = CacheableMetadata::createFromRenderArray([]);
-
-    /** @var \Drupal\Core\Layout\LayoutInterface $layout */
-    $layout = $this->layoutPluginManager->createInstance($item->layout);
-    $regions = [];
-    foreach ($item->section as $region => $blocks) {
-      foreach ($blocks as $uuid => $configuration) {
-        /** @var \Drupal\Core\Block\BlockPluginInterface $block */
-        $block = $this->blockManager->createInstance($configuration['plugin_id'], $configuration);
-        if ($block instanceof ContextAwarePluginInterface) {
-          $contexts = $this->contextRepository->getRuntimeContexts(array_values($block->getContextMapping()));
-          $this->contextHandler->applyContextMapping($block, $contexts);
-        }
-        $access = $block->access($this->account, TRUE);
-        $cacheability->addCacheableDependency($access);
-        if ($access->isAllowed()) {
-          $regions[$region][$uuid] = $block->build();
-          $cacheability->addCacheableDependency($block);
-        }
-      }
-    }
-    $section = $layout->build($regions);
-    $cacheability->applyTo($section);
-    return $section;
   }
 
 }
