55ade207d848028d759305759b0bb32bce6d6a14
Adam Hoenich <adam.hoenich@acquia.com>
2017-05-25 12:06:18 -0400

    Removed errant line from outside_in.js

--- core/modules/outside_in/js/outside_in.js
+++ core/modules/outside_in/js/outside_in.js
@@ -161,7 +161,6 @@
         var $activeElement = $('#' + settings.outsideInActiveEditableId);
         if ($activeElement.length) {
           $activeElement.addClass('outside-in-active-editable');
-          settings.dialogClass += ' ui-dialog-outside-in';
         }
       }
     },
