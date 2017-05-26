cf560c233457c05ce51e99eb5ee7ce7c61f50585
Tim Plunkett <git@plnktt.com>
2017-05-25 23:55:39 -0400

    Make draggable code save the changes

--- core/modules/layout_builder/js/layout-builder.js
+++ core/modules/layout_builder/js/layout-builder.js
@@ -5,7 +5,32 @@
     attach: function (context) {
       $(context).find('.layout__region').sortable({
         items: '> .draggable',
-        connectWith: '.layout__region'
+        connectWith: '.layout__region',
+        update: function (event, ui) {
+          let data = {
+            region_to: $(this).data('region'),
+            block_uuid: ui.item.data('layout-block-uuid'),
+            delta_to: ui.item.closest('[data-layout-delta]').data('layout-delta'),
+            preceding_block_uuid: ui.item.prev('[data-layout-block-uuid]').data('layout-block-uuid')
+          };
+          if (this === ui.item.parent()[0]) {
+            if (ui.sender) {
+              data.region_from = ui.sender.data('region');
+              data.delta_from = ui.sender.closest('[data-layout-delta]').data('layout-delta');
+            }
+            else {
+              data.region_from = data.region_to;
+              data.delta_from = data.delta_to;
+            }
+
+            let url = ui.item.closest('[data-layout-update-url]').data('layout-update-url');
+
+            $.ajax(url, {
+              data: JSON.stringify(data),
+              method: 'POST'
+            });
+          }
+        }
       });
     }
 
