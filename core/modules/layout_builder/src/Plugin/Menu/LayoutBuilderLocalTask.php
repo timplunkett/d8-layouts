5e5948218c2b4f21f1bcfa85cfdf14d1d65f273b
Tim Plunkett <git@plnktt.com>
2017-06-16 10:06:55 -0400

    Refactor to avoid calling the enhancer manually.

---
+++ core/modules/layout_builder/src/Plugin/Menu/LayoutBuilderLocalTask.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\layout_builder\Plugin\Menu;
+
+use Drupal\Core\Menu\LocalTaskDefault;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Provides route parameters needed to link to layout related tabs.
+ */
+class LayoutBuilderLocalTask extends LocalTaskDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteParameters(RouteMatchInterface $route_match) {
+    $parameters = parent::getRouteParameters($route_match);
+
+    $parameters['layout_section_entity'] = $route_match->getParameter('layout_section_entity');
+    $parameters['layout_section_field_name'] = $route_match->getParameter('layout_section_field_name');
+    return $parameters;
+  }
+
+}
