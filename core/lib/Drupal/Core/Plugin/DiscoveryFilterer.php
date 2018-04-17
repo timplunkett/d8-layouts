14336d8d3c0c5cff69c600a63574732e409c396d
Tim Plunkett <git@plnktt.com>
2018-04-16 21:08:17 -0400

    Expand to themes

--- core/lib/Drupal/Core/Plugin/DiscoveryFilterer.php
+++ core/lib/Drupal/Core/Plugin/DiscoveryFilterer.php
@@ -5,6 +5,7 @@ namespace Drupal\Core\Plugin;
 use Drupal\Component\Plugin\Discovery\DiscoveryInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\Context\ContextHandlerInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
 
 /**
  * Provides methods to retrieve filtered plugin definitions.
@@ -25,6 +26,13 @@ class DiscoveryFilterer {
   protected $moduleHandler;
 
   /**
+   * The theme manager.
+   *
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
    * The context handler.
    *
    * @var \Drupal\Core\Plugin\Context\ContextHandlerInterface
@@ -36,11 +44,14 @@ class DiscoveryFilterer {
    *
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
+   *   The theme manager.
    * @param \Drupal\Core\Plugin\Context\ContextHandlerInterface $context_handler
    *   The context handler.
    */
-  public function __construct(ModuleHandlerInterface $module_handler, ContextHandlerInterface $context_handler) {
+  public function __construct(ModuleHandlerInterface $module_handler, ThemeManagerInterface $theme_manager, ContextHandlerInterface $context_handler) {
     $this->moduleHandler = $module_handler;
+    $this->themeManager = $theme_manager;
     $this->contextHandler = $context_handler;
   }
 
@@ -72,6 +83,7 @@ class DiscoveryFilterer {
     $hooks[] = "plugin_filter_{$type}";
     $hooks[] = "plugin_filter_{$type}__{$consumer}";
     $this->moduleHandler->alter($hooks, $definitions, $extra, $consumer);
+    $this->themeManager->alter($hooks, $definitions, $extra, $consumer);
     return $definitions;
   }
 
